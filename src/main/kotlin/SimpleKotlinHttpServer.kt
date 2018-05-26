553154a897eb1d38c29fbfc58bb72fcd5083b77c
tomohiro.sano <36667265+sanopi@users.noreply.github.com>
2018-05-26 16:30:05 +0900

    creating servet socket listening on localhost:8000

--- src/main/kotlin/SimpleKotlinHttpServer.kt
+++ src/main/kotlin/SimpleKotlinHttpServer.kt
@@ -1,6 +1,18 @@
+import utils.Constants.Companion.PORT
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+
 /**
  * @author TomohiroSano
  */
+
+
 fun main(args: Array<String>) {
-        println("Hello World")
+    ServerSocket().use { serverSocket ->
+        serverSocket.bind(InetSocketAddress(InetAddress.getLoopbackAddress(), PORT))
+        println("listening HTTP request on ${serverSocket.inetAddress} port ${serverSocket.localPort} ...")
+        val socket = serverSocket.accept()
+        println("Hello Request!")
+    }
 }
---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,10 @@
+package utils
+
+/**
+ * @author TomohiroSano
+ */
+class Constants {
+    companion object {
+        const val PORT = 8000
+    }
+}
