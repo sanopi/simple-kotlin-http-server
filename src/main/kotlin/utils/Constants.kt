eb74719f417016c500765a09aeb70ed9126f81a1
tomohiro.sano <36667265+sanopi@users.noreply.github.com>
2018-05-31 00:41:21 +0900

    reply as simple HTTP response

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,11 @@
+package utils
+
+/**
+ * @author TomohiroSano
+ */
+class Constants {
+    companion object {
+        const val CRLF: String = "\r\n"
+        const val HTTP_VERSION: String = "HTTP/1.1"
+    }
+}
